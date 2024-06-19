97b3973e210d3e6e6b10278156196d73137c83fc
Jemish Kumar <47361171+JemishViradiya@users.noreply.github.com>
2024-06-19 15:48:32 -0400

    Add files via upload

---
+++ frontend/transaction-portal/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TransactionListComponent } from './transaction-list/transaction-list.component';
+
+const routes: Routes = [
+  { path: 'transactions', component: TransactionListComponent },
+  { path: '', redirectTo: '/transactions', pathMatch: 'full' },
+  { path: '**', redirectTo: '/transactions' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
---
+++ frontend/transaction-portal/src/app/models/transaction.ts
@@ -0,0 +1,6 @@
+export interface Transaction {
+  id: string;
+  date: string;
+  comments: string;
+  action: string;
+}
